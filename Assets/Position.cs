e97e1ea2d883cbeda131abb500471314f99a376f
Niyati Shah <niyati@Niyatis-MacBook-Air.local>
2018-04-26 18:06:04 -0400

    First commit

---
+++ Assets/Position.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Position : MonoBehaviour {
+
+    public GameObject right;
+    public GameObject left;
+    public GameObject center;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        right.transform.position = new Vector3(25f, 3f, 9f);
+
+    }
+}
---
+++ Assets/Webcam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webcam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            print("webcam: " + devices[i].name);
+        }
+
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+        WebCamTexture tex = new WebCamTexture(devices[0].name);
+        rend.material.mainTexture = tex;
+        tex.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
